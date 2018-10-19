cd418df6a1e575181ba78180f3b63d936285b680
vikas <cseghrce@gmail.com>
2018-10-20 04:22:04 +0530

    First Commit

--- src/basic/multivariable.py
+++ src/basic/multivariable.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
+import statsmodels.api as sm
+import seaborn
 def run():
     mul_var()
     pass
@@ -24,9 +26,16 @@ def mul_var():
     print("intercept : ", model.intercept_)
     print("mean square : ", mean_squared_error(test_y, predicted))
     print("variace : ", r2_score(test_y, predicted))
-
+    model_1 = sm.OLS(train_y, train_x)
+    result = model_1.fit()
+    print(result.summary())
+    corr_df = train_x.corr(method='pearson')
+    mask = np.zeros_like(corr_df)
+    mask[np.triu_indices_from(mask)] = True
+    seaborn.heatmap(corr_df, cmap='RdYlGn_r', vmax=1.0, vmin=1.0, mask= mask, linewidths=2.5)
+    plt.yticks(rotation=0)
+    plt.xticks(rotation=90)
     plt.show()
     print('Okay!')
-    plt.show()
 
 
